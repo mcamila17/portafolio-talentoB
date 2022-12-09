00663c9392beee8dce776886acd3367df0d0c1e2
Maria Camila Troncoso <115896872+mcamila17@users.noreply.github.com>
2022-12-08 20:24:06 -0500

    Add files via upload

---
+++ build/js/nav.js
@@ -0,0 +1,2 @@
+function eventListeners(){document.querySelector(".mobile-menu").addEventListener("click",navegacionResponsive)}function navegacionResponsive(){document.querySelector(".navegacion-principal").classList.toggle("mostrar")}document.addEventListener("DOMContentLoaded",(function(){eventListeners()}));
+//# sourceMappingURL=nav.js.map
